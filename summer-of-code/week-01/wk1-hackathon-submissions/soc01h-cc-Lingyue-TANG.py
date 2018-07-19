03316aa31ca1b4bd2e126689206edd187f1c0cf2
TangLy37 <41243120+TangLy37@users.noreply.github.com>
2018-07-20 01:09:46 +0200

    Add files via upload

---
+++ summer-of-code/week-01/wk1-hackathon-submissions/soc01h-cc-Lingyue-TANG.py
@@ -0,0 +1,17 @@
+## Summer of code
+## Lingyue TANG
+## July 20,2018
+## Week1-homework submission
+
+print ("hours in a year")
+print (365*24)
+
+print ("minutes in a year")
+print (365*24*60)
+
+print ("seconds in my age")
+print (27*365*24*60*60)
+
+first= input("What is your first name?")
+last= input("What is your family name?")
+print("Hello"+first+last+"!")
